d3ff888d80ffbba57db87bd456acc2d7bdedeee1
raghavrawat <raghav@symboinsurance.com>
2021-02-21 17:24:31 +0530

    chnages of the dropdown and checkboxes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import CustomSelectDropdown from './CustomSelectDropdown';
+import { useEffect, useState } from 'react';
 
-function App() {
+const renderedData = {
+  Colours: ['red', 'yellow', 'green', 'blue'],
+  Components: [
+      {
+          'title': 'Button', 'path': 'demo-button', 'value': 'Button', 'label': 'Button'
+      },
+      {
+          'title': 'Selection Control', 'path': 'demo-selection-control', 'value': 'Selection Control', 'label': 'Selection Control'
+      },
+      {
+          'title': 'Input', 'path': 'demo-input', 'value': 'Input', 'label': 'Input'
+      },
+      {
+          'title': 'Snackbar', 'path': 'demo-snackbar', 'value': 'Snackbar', 'label': 'Snackbar'
+      },
+      {
+          'title': 'Chips', 'path': 'demo-chips', 'value': 'Chips', 'label': 'Chips'
+      },
+      {
+          'title': 'Progress Tabs', 'path': 'demo-vertical-tabs', 'value': 'Progress Tabs', 'label': 'Progress Tabs'
+      },
+      {
+          'title': 'Typography', 'path': 'demo-wip', 'value': 'Typography', 'label': 'Typography'
+      },
+      {
+          'title': 'Card', 'path': 'demo-wip', 'value': 'Card', 'label': 'Card'
+      },
+      {
+          'title': 'Pagination', 'path': 'demo-wip', 'value': 'Pagination', 'label': 'Pagination'
+      }
+  ]
+}
+
+const App = () => {
+
+  const [defaultOptions, setDefaultOptions] = useState([]);
+  const [dropdownOptions, setDropdownOptions] = useState([]);
+
+  const formatData = data => {
+    let returnObj = {};
+    Object.keys(data).forEach(obj => {
+      const itemArr = data[obj];
+      if(typeof itemArr[0] === 'object') {
+        const newItemArr = itemArr.map(item => {
+          return item.title;
+        })
+        returnObj[obj] = newItemArr;
+      }
+      else {
+        returnObj[obj] = itemArr;
+      }
+    })
+    return returnObj;
+  }
+
+  useEffect(() => {
+    const defaultOptions = Object.keys(renderedData).map(item => {
+       return {label: item, value: item} 
+    })
+    const data = formatData(renderedData);
+    setDropdownOptions(data)
+    setDefaultOptions(defaultOptions)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='custom-select-container'>
+      <h3 style={{fontWeight: 900, color: 'black'}}>Dropdown with search</h3>
+      <CustomSelectDropdown 
+        data={dropdownOptions} 
+        defaultOptions={defaultOptions} 
+        searchable={true} 
+        multiselect={true}
+        defaultValue={'Colours'}
+      />
     </div>
   );
 }
---
+++ src/CustomSelectDropdown.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from 'react';
+import { Select, Input, Checkbox } from 'antd';
+import './App.css'
+import _ from 'lodash';
+
+const { Option } = Select;
+
+const CheckboxGroup = Checkbox.Group;
+
+const CustomSelectDropdown = ({
+    data,
+    defaultOptions,
+    searchable,
+    multiselect,
+    defaultValue
+}) => {
+    const [checkboxOptions, setCheckboxOptions] = useState();
+    const [checkedList, setCheckedList] = useState([]);
+    const [indeterminate, setIndeterminate] = useState(false);
+    const [checkAll, setCheckAll] = useState(false);
+    const [ekVal, setEkVal] = useState(defaultValue);
+    const [resultValue, setResultValue] = useState();
+
+    const generateOptionField = (options) => {
+        return options.map(item => {
+            return <Option key={item.value} value={item.value}>{item.label}</Option>
+        });
+    }
+
+    useEffect(() => {
+        setCheckboxOptions(data['Colours']);
+    }, [data])
+
+    const onHandleChange = (value) => {
+        setEkVal(value);
+        setCheckboxOptions(data[value]);
+        setResultValue();
+        setCheckAll(false);
+        setIndeterminate(false);
+        setCheckedList([])
+    }
+
+    const onChange = (list) => {
+        setCheckedList(list);
+        setIndeterminate(!!list.length && list.length < data[ekVal].length);
+        setCheckAll(list.length === data[ekVal].length);
+      };
+    
+    const onCheckAllChange = e => {
+        setCheckedList(e.target.checked ? data[ekVal] : []);
+        setIndeterminate(false);
+        setCheckAll(e.target.checked);
+    };
+
+    const onInputChange = (e) => {
+        const { value } = e.target;
+        if(data[ekVal]) {
+            const itemFiltered = data[ekVal].map(item => {
+                if (item.toLowerCase().indexOf(value.toLowerCase()) > -1) {
+                    return item;
+                }
+            }).filter(i => i !== undefined);
+            setCheckboxOptions(itemFiltered);
+        }
+    }
+
+    const onSubmit = () => {
+        const resultString = Array.isArray(checkedList) ? checkedList.join(',') : checkedList;
+        const resultValue = `${ekVal} - ${resultString}`;
+        setResultValue(resultValue);
+    }
+
+    const singleClick = (value) => {
+        setCheckedList(value);
+    }
+
+    const generateDivElements = (data) => {
+        return (
+            <div className='checkboxGroup'>
+                {data.map((item, index) => {
+                    return <div key={index} className='ant-checkbox-group-item' onClick={() => singleClick(item)} style={{ cursor: 'pointer'}}>{item}</div>
+                })}
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <div style = {{paddingTop: '20px', lineHeight: '2.5rem'}}>
+                <div>
+                    <label>Default</label>
+                </div>
+                <div>
+                    <Select defaultValue="Colours" style={{ width: 200 }} onChange={onHandleChange}>
+                        {generateOptionField(defaultOptions)}
+                    </Select>
+                </div>
+            </div>
+            <div style = {{paddingTop: '20px', lineHeight: '2.5rem'}}>
+                <div>
+                    <label>Dropdown</label>
+                </div>
+                {searchable && <div>
+                    <Input placeholder="Search" style={{width: 200, height: 40}} onChange={onInputChange}/>
+                </div>}
+                <div className='checkbox-container'>
+                    { multiselect ?
+                    <>
+                    <Checkbox style={{width: 200, height: 40,borderBottom: '1.5px solid #EEEEEE', paddingLeft: 10}} indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}/>
+                    <CheckboxGroup className='checkboxGroup' style={{display: 'flex', flexDirection: 'column', width: 200, lineHeight: 'inherit'}} options={checkboxOptions} onChange={onChange} value={checkedList} />
+                    </>
+                    :
+                    generateDivElements(checkboxOptions)
+                    }<div style={{ borderTop: '1.5px solid #eeeeee', display: "flex", justifyContent: 'flex-end', alignItems: "center", height: 60, paddingRight: 10}}>
+                        <div style={{paddingRight: 30, cursor: 'pointer'}}>Clear</div>
+                        <div style={{color: "#6CC69D", fontWeight: 900, cursor: 'pointer'}} onClick={onSubmit}>Submit</div>
+                    </div>
+                </div>
+            </div>
+            <div style = {{paddingTop: '20px', lineHeight: '2.5rem'}}>
+                <div>
+                    <label>Result</label>
+                </div>
+                <Input style={{width: 200, textOverflow: 'ellipsis'}} value={resultValue}/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default CustomSelectDropdown
